de8c4d04cc90572469f414c78a5bf0ef78eb6872
j1zuz <101004975+j1zuzx@users.noreply.github.com>
2024-01-05 17:51:26 -0500

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://discord.movie-web.app";
-export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
-export const DONATION_LINK = "https://ko-fi.com/movieweb";
+export const DISCORD_LINK = "https://t.me/Hackw_v0";
+export const GITHUB_LINK = "https://github.com/j1zuzx";
+export const DONATION_LINK = "https://paypal.me/JesusL724";
 export const GA_ID = "G-44YVXRL61C";
 export const BACKEND_URL = "https://backend.movie-web.app";
